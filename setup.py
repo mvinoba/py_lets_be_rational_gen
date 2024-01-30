dca149604a182c2bb79273d3b2579e85179e5a4b
Vinícius Figueiredo <vinoba_@hotmail.com>
2024-01-30 09:46:40 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='MIT',
     maintainer='',
     maintainer_email='',
-    long_description=open('README.md').read(),
+    long_description='',
     description='Pure python implementation of Peter Jaeckel\'s LetsBeRational.',
     install_requires = [
         'numpy'
